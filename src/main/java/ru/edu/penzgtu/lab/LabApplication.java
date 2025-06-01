0ed6b5f4736224970caadb942a3b27afa2cc9b26
Михаил <parkurhack@gmail.com>
2025-06-01 18:51:16 +0300

    Зависимости и аннотации для документации Spring

--- src/main/java/ru/edu/penzgtu/lab/LabApplication.java
+++ src/main/java/ru/edu/penzgtu/lab/LabApplication.java
@@ -1,9 +1,24 @@
 package ru.edu.penzgtu.lab;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "PenzGTU Java Lab API",
+				description = "API for labs",
+				version = "1.0.0",
+				contact = @Contact(
+					name = "Ильяс Акбулатов"
+				)
+		)
+)
 @SpringBootApplication
+@EnableWebMvc
 public class LabApplication {
 
 	public static void main(String[] args) {
