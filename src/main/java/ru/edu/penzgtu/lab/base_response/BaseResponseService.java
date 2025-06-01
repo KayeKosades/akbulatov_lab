2e72b345e3e56505581a37a59d92aa839675927d
Михаил <parkurhack@gmail.com>
2025-06-01 20:32:33 +0300

    Классы для dto ошибки и респонзов сервера, которые оборачивают ошибки

---
+++ src/main/java/ru/edu/penzgtu/lab/base_response/BaseResponseService.java
@@ -0,0 +1,30 @@
+package ru.edu.penzgtu.lab.base_response;
+
+import org.springframework.stereotype.Service;
+import ru.edu.penzgtu.lab.exception.PenzGtuException;
+
+@Service
+public class BaseResponseService {
+
+    public <T> ResponseWrapper<T> wrapSuccessResponse(T body) {
+        return ResponseWrapper.<T>builder()
+                .success(true)
+                .body(body)
+                .error(null)
+                .build();
+    }
+
+    public ResponseWrapper<?> wrapErrorResponse(PenzGtuException exception) {
+        ErrorDto error = ErrorDto.builder()
+                .code(exception.getType().name())
+                .title(exception.getType().getTitle())
+                .text(exception.getType().getText())
+                .build();
+
+        return ResponseWrapper.builder()
+                .success(false)
+                .body(null)
+                .error(error)
+                .build();
+    }
+}
